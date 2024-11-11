9c5616d4f836ab951e0d55fd6a96881738f051eb
Ryota Funaki <ryota.funaki@ngv.jp>
2024-11-11 03:43:56 +0000

    Add Rust

--- shells/root/shells.d/install_serve.sh
+++ shells/root/shells.d/install_serve.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Copyright (c) 2024 RFull Development
 # This source code is managed under the MIT license. See LICENSE in the project root.
-npm install -g create-next-app
+npm install -g serve
 if [ $? -ne 0 ]; then
-    echo "Failed to install create-next-app"
+    echo "Failed to install serve"
     exit 1
 fi
 exit 0
